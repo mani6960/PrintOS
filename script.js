7a63a12e5e0bec0e00b4bff4ec67d4d6b086b5f4
mani6960 <127227555+mani6960@users.noreply.github.com>
2026-07-29 12:20:37 +0530

    Implement file upload and display functionality
    Add file input handling and display file details

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 
+const fileInput = document.getElementById("fileInput");
+const fileCard = document.getElementById("fileCard");
+const fileName = document.getElementById("fileName");
+const fileSize = document.getElementById("fileSize");
+const continueBtn = document.getElementById("continueBtn");
+
+fileInput.addEventListener("change", () => {
+
+    if(fileInput.files.length === 0) return;
+
+    const file = fileInput.files[0];
+
+    fileCard.style.display = "block";
+
+    fileName.innerHTML = "📄 " + file.name;
+
+    fileSize.innerHTML =
+        "Size : " + (file.size / 1024 / 1024).toFixed(2) + " MB";
+
+    continueBtn.disabled = false;
+    continueBtn.style.opacity = "1";
+});
+
+continueBtn.addEventListener("click", () => {
+    alert("Next Step: PDF Preview");
+});
